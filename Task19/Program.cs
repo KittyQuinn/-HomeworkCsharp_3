6745e4106f68d8f462289700808c5837278faf58
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-14 20:26:21 +0200

    Added task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Type 5-digit number ");
+int num = int.Parse(Console.ReadLine());
+
+string str = num.ToString();
+
+if ((str[0] == str[4]) & (str[1] == str[3]))
+Console.WriteLine("Yes");
+
+else
+Console.WriteLine("No");
