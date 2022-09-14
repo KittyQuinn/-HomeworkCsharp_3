6694ffd085c43b7475f131b2f79fd22a2eb7395d
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-14 20:48:41 +0200

    Added task23

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 23
+// Напишите программу, 
+// которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+
+Console.Clear();
+Console.WriteLine("Type number ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1 ; i <= num ; i++)
+Console.WriteLine(Math.Pow(i, 3));
